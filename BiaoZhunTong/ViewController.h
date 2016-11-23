472a05870b0178a278633e5e2db204f11e5cd764
txx <deng@mbp13-ybzf.local>
2016-11-23 16:55:43 +0800

    init project

---
+++ BiaoZhunTong/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BiaoZhunTong
+//
+//  Created by txx on 16/11/23.
+//  Copyright © 2016年 txx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BiaoZhunTong/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  BiaoZhunTong
+//
+//  Created by txx on 16/11/23.
+//  Copyright © 2016年 txx. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
